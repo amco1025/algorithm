290982b0acb00e4b428cae1de7f1eb42b1a7e847
seonjae <119836719+amco1025@users.noreply.github.com>
2023-02-21 17:46:07 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 251 ms, Memory: 55,672 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,33 +1,37 @@
-from collections import deque
- 
-def bfs(a, b):
-  dx = [0,0,-1,1]
-  dy = [1,-1,0,0]
-  q = deque()
-  q.append([a,b])
-  visited[a][b] = 1
- 
-  while q:
-    x,y = q.popleft()
-     
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
- 
-      if 0<=nx<16 and 0<=ny<16:
-        if maze[nx][ny] == 3:
-          return 1
- 
-        if maze[nx][ny] == 0 and not visited[nx][ny]:
-          visited[nx][ny] = True
-          q.append([nx,ny])
-   
-  return 0
- 
- 
-for tc in range(10):
-  n = int(input())
-  maze = [list(map(int, input())) for _ in range(16)]
-  visited = [[False for _ in range(16)] for _ in range(16)]
-  answer = bfs(1, 1)     
-  print(f'#{n} {answer}')
+def BFS(arr, si, sj): 
+    queue = []
+    queue.append((si,sj))
+    ci, cj = si, sj
+    visited[ci][cj] = 1
+    while queue:
+        ci, cj = queue.pop(0)
+        for di, dj in ((1,0),(-1,0),(0,1),(0,-1)):
+            ni = ci + di
+            nj = cj + dj
+            if 0<=ni<16 and 0<=nj<16 and arr[ni][nj] != 1:
+                if visited[ni][nj] == 0:
+                    visited[ni][nj] = 1
+                    queue.append((ni,nj))
+    return visited
+
+
+T = 10
+
+for tc in range(1, T+1):
+    n = int(input())
+    arr = [list(map(int, input())) for _ in range(16)]
+
+    for i in range(16):
+        for j in range(16):
+            if arr[i][j] == 2:
+                si, sj = i, j
+
+    for i in range(16):
+        for j in range(16):
+            if arr[i][j] == 3:
+                ei, ej = i, j
+
+    visited = [[0] * 16 for _ in range(16)]
+
+    answer = BFS(arr, si, sj)
+    print("#"+str(tc)+" "+str(answer[ei][ej]))
