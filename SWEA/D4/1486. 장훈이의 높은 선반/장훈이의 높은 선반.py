31aaee53c11e7b0e34b06bb1d070356bdffc673d
seonjae <119836719+amco1025@users.noreply.github.com>
2023-03-28 19:00:35 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 263 ms, Memory: 63,304 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,23 +1,26 @@
-T = int(input()) # 테스트 케이스 수 입력
+def dfs(n,h):
+    global ans
+    # 가지 치기
+    if h-H > ans:
+        return
+	# 종료 조건
+    if n == N:
+        if h-H >= 0:
+            if h-H < ans:
+                ans = h - H
+        return
+	# 하부 조건
+    dfs(n+1, h)
+    dfs(n+1, h + li[n])
 
-for tc in range(1, T+1): # 반복
-    N, B = map(int, input().split())     # 사람 수와 높이 입력
-    li = list(map(int, input().split())) # 사람의 키 리스트 형식으로 저장
-    n = len(li)
-    answer = []
-    final = []
-    answer_1 = 0
-    for i in range(1, 1<<n): # 부분집합의 합을 구하며 나올 수 있는 경우의 합 모두 저장
-        sm = 0
-        for j in range(n):
-            if i & (1 << j):
-                sm += li[j]
-        answer.append(sm)
 
-    for i in answer:  # 차외 값 저장
-        if i >= B:
-            final.append(i-B)
+T = int(input())
+for tc in range(1, T+1):
+    N, H = map(int, input().split())
+    li = list(map(int, input().split()))
 
-    answer_1 = min(final) # 차의 최소값 저장
-    
-    print("#"+str(tc)+" "+str(answer_1))
+    ans = 10000
+
+    dfs(0,0)
+
+    print("#"+str(tc)+" "+str(ans))
