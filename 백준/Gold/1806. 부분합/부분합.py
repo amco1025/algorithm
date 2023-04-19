4478ed145ba444bd478849b2a55bdd17b2ab0fec
seonjae <119836719+amco1025@users.noreply.github.com>
2023-04-19 18:51:53 +0900

    [Gold IV] Title: 부분합, Time: 180 ms, Memory: 125300 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,32 @@
+import sys
+
+N, S = map(int, input().split())
+li = list(map(int, input().split()))
+
+si = 0
+ei = 0
+
+ans = sys.maxsize
+sm = 0
+
+while True:
+    if sm >= S:
+        ans = min(ans, ei - si)
+        sm -= li[si]
+        si += 1
+
+    elif ei == N:
+        break
+
+    # elif ans == 1:
+    #     break
+
+    else:
+        sm += li[ei]
+        ei += 1
+
+
+if ans == sys.maxsize:
+    print(0)
+else:
+    print(ans)
