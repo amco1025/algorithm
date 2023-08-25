ef2e36963220acb9fc34a94a400331d6abe582f5
seonjae <119836719+amco1025@users.noreply.github.com>
2023-08-25 13:32:31 +0900

    [Gold IV] Title: 연구소, Time: 1316 ms, Memory: 117716 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,48 +1,46 @@
-from itertools import combinations
-import copy
-from collections import deque
-
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-
-candidates = []
-virus = []
-wall = 0
-for i in range(N):
-    for j in range(M):
-        if arr[i][j] == 0:
-            candidates.append((i,j))
-
-        elif arr[i][j] == 2:
-            virus.append((i,j))
-
-
-
-tmp_walls = combinations(candidates, 3)
-ans = 0
-for tmp_wall in tmp_walls:
-    sm =0
-    grid = copy.deepcopy(arr)
-    for i, j in tmp_wall:
-        grid[i][j] = 1
-
-    now_virus = deque(virus)
-
-    while now_virus:
-        x, y = now_virus.popleft()
-
-        for di, dj in ((1,0),(-1,0),(0,1),(0,-1)):
-            nx = x + di
-            ny = y + dj
-            if 0<=nx<N and 0<=ny<M and grid[nx][ny] == 0:
-                grid[nx][ny] = 2
-                now_virus.append((nx,ny))
-
-    for i in range(N):
-        for j in range(M):
-            if grid[i][j] == 0:
-                sm += 1
-
-    ans = max(sm, ans)
-
-print(ans)
+s = []
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+max_result = 0
+def bfs():
+    global max_result
+    copy = [[0] * m for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            copy[i][j] = s[i][j]
+    result = 0
+    arr = []
+    for i in range(n):
+        for j in range(m):
+            if copy[i][j] == 2:
+                arr.append([i, j])
+    while arr:
+        a, b = arr[0][0], arr[0][1]
+        del arr[0]
+        for i in range(4):
+            ax = a + dx[i]
+            ay = b + dy[i]
+            if 0 <= ax and 0 <= ay and ax < n and ay < m:
+                if copy[ax][ay] == 0:
+                    copy[ax][ay] = 2
+                    arr.append([ax, ay])
+    for i in copy:
+        for j in i:
+            if j == 0:
+                result += 1
+    max_result = max(max_result, result)
+def wall(cnt):
+    if cnt == 3:
+        bfs()
+        return
+    for i in range(n):
+        for j in range(m):
+            if s[i][j] == 0:
+                s[i][j] = 1
+                wall(cnt + 1)
+                s[i][j] = 0
+n, m = map(int, input().split())
+for i in range(n):
+    s.append(list(map(int, input().split())))
+wall(0)
+print(max_result)
